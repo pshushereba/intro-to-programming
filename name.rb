153e1de9f6f8e51bab94a35f1fe4ab1a2cbf3357
Patrick Shushereba <pshushereba@gmail.com>
2016-04-06 17:12:50 -0700

    Finished Variable section of book.

---
+++ name.rb
@@ -0,0 +1,11 @@
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+
+puts "Nice to meet you, #{first_name + " " + last_name}!"
+
+10.times do
+  puts first_name
+end
+
---
+++ variables-exercises.rb
@@ -0,0 +1,5 @@
+# 5. The first program prints 3 to the screen. The second program
+# gives an error.
+
+# 6. The error comes up because we haven't defined the variable, or someone
+# was trying to access the variable outside of the scope.
